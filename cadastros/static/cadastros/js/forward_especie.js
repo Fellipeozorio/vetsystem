9e3746a5d0ae32aeae7ce7976c71959de18089d9
Fellipe Alves Ozorio do Nascimento <fellipe.nascimento@zenithmaritima.local>
2026-02-05 14:31:19 -0300

    Make Raca autocomplete forwarding more robust (broaden URL matching)

--- cadastros/static/cadastros/js/forward_especie.js
+++ cadastros/static/cadastros/js/forward_especie.js
@@ -2,19 +2,20 @@
   $(function(){
     // Prefilter AJAX requests to append the selected especie to raca autocomplete calls
     $.ajaxPrefilter(function(options, originalOptions, jqXHR){
-      var url = options.url || '';
-      // target the admin autocomplete endpoint for cadastros.raca
-      if (url.indexOf('/admin/cadastros/raca/autocomplete/') !== -1) {
-        // try common id for especie field in admin forms
-        var especieVal = $('#id_especie').val();
-        if (!especieVal) {
-          // fallback: try name-based selectors used in inlines/forms
-          especieVal = $('[name$="especie"]').first().val();
-        }
-        if (especieVal) {
-          var sep = url.indexOf('?') === -1 ? '?' : '&';
-          options.url = url + sep + 'especie=' + encodeURIComponent(especieVal);
-        }
+      var url = (options.url || '').toString();
+      // target requests that look like admin autocomplete for cadastros.Raca
+      var isRacaAutocomplete = /cadastros\/.{0,50}raca/i.test(url) && (/autocomplete/i.test(url) || /term=/.test(url) || /q=/.test(url));
+      if (!isRacaAutocomplete) return;
+
+      // try common id for especie field in admin forms
+      var especieVal = $('#id_especie').val();
+      if (!especieVal) {
+        // fallback: try name-based selectors used in inlines/forms
+        especieVal = $('[name$="especie"]').first().val();
+      }
+      if (especieVal) {
+        var sep = url.indexOf('?') === -1 ? '?' : '&';
+        options.url = url + sep + 'especie=' + encodeURIComponent(especieVal);
       }
     });
   });
